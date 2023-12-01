f19ab507cab1f54226282d3e09fb84eaa4dd1e5a
Naveen Kumar <134830093+nvnKr09@users.noreply.github.com>
2023-12-01 20:10:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const div = document.getElementById('browser-info');
+
+div.innerHTML = `
+	You are using ${navigator.appName} ${navigator. appVersion} ${navigator.userAgent}.
+`
