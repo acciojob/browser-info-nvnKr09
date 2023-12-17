2ccdfec15e57232b1c15be91cb56eddbd7820123
Naveen Kumar <134830093+nvnKr09@users.noreply.github.com>
2023-12-17 23:16:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 //your JS code here. If required.
-const div = document.getElementById('browser-info');
-
-div.innerHTML = `
-	You are using ${navigator.appName} ${navigator. appVersion} ${navigator.userAgent}.
-`
+let browserName = navigator.appName;
+let version = navigator.appVersion;
+document.getElementById("browser-info").innerHTML = "You are using " + browserName + " version " + version;
